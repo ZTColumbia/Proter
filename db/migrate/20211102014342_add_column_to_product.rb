92df65647274094582e6297eb1b14ed6ffcc7d4c
tomstark <zhantong@sensetime.com>
2021-11-01 21:44:21 -0400

    [add] name column

---
+++ db/migrate/20211102014342_add_column_to_product.rb
@@ -0,0 +1,5 @@
+class AddColumnToProduct < ActiveRecord::Migration
+  def change
+    add_column :products, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211102000150) do
+ActiveRecord::Schema.define(version: 20211102014342) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20211102000150) do
     t.string   "box"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
