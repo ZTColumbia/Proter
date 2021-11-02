376c0dc55c3a8dc8b662f7806b1e22ab3e12f5e7
Dechen Zeng <dz2455@columbia.edu>
2021-11-02 04:03:30 +0000

    fix product controller and added product pages

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,42 @@
+class ProductsController < ApplicationController
+  def show
+    id = params[:id]
+    @product = Product.find(id)
+  end
+
+  def create
+    @product = Product.create!(product_params)
+    flash[:notice] = "#{@product.name} was successfully created."
+    redirect_to products_path
+  end
+
+  def update
+    @product = Product.find params[:id]
+    @product.update_attributes!(product_params)
+    flash[:notice] = "#{@product.name} was successfully updated."
+    redirect_to products_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product '#{@product.name}' deleted."
+    redirect_to products_path
+  end
+    
+  def edit
+    @product = Product.find params[:id]
+  end
+
+  def index
+    @products = Product.all
+  end
+    
+  def new
+      
+  end
+    
+  def product_params
+    params.require(:product).permit(:product_id, :name, :seller_id, :brand, :price, :weight, :color, :discount, :category, :box, :created_at, :updated_at)
+  end
+end
