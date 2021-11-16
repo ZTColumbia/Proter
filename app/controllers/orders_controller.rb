babadb7af1178b7729302c180a829127b10e288d
Dechen Zeng <zengdechen@gmail.com>
2021-11-16 13:15:21 -0500

    added draft page for order history

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
   def show
-    user_id = params[:user_id]
+    user_id = current_user.id
     @orders = Order.where(user_id: user_id)
     render :json => @orders
   end
