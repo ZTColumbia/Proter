940b72c61681474cb99dcfff164a3b4b7f2655e1
Yuantai Xu <yx2623@columbia.edu>
2021-11-18 16:59:07 +0000

    fix something

--- spec/user_controller_spec.rb
+++ spec/user_controller_spec.rb
@@ -5,11 +5,10 @@ require 'clearance/rspec'
 RSpec.describe UsersController, type: 'controller' do
     describe "GET #show" do
         before :each do
-          User.create(email: 'Bob@gmail.com', password: "bobalice", first_name: "Alice", last_name: "Bob", city: "New York", state: "NY", address: "100 W, 100 ST", card_number: "1111 1111 1111 1111", card_cvv: "111", card_expired_date: "2021.11.4")
-          @user = User.all
+          sign_in
         end
         it "should post new test user" do
-        get :show, id: @user.take.id
+        get :show, id: 1
             expect(response).to be_truthy
         end
     end
@@ -23,22 +22,30 @@ RSpec.describe UsersController, type: 'controller' do
   
     describe "GET #edit" do
         before :each do
-          User.create(email: 'Bob@gmail.com', password: "bobalice", first_name: "Alice", last_name: "Bob", city: "New York", state: "NY", address: "100 W, 100 ST", card_number: "1111 1111 1111 1111", card_cvv: "111", card_expired_date: "2021.11.4")
-          @user = User.all
+          sign_in
         end
         it "should post new test user" do
-        get :edit, id: @user.take.id
+        get :edit, id: 1
             expect(response).to be_truthy
         end
     end
   
    describe "GET #profile" do
         before :each do
-          User.create(email: 'Bob@gmail.com', password: "bobalice", first_name: "Alice", last_name: "Bob", city: "New York", state: "NY", address: "100 W, 100 ST", card_number: "1111 1111 1111 1111", card_cvv: "111", card_expired_date: "2021.11.4")
-          @user = User.all
+          sign_in
         end
         it "should post new test user" do
-        get :profile, id: @user.take.id
+        get :profile, id: 1
+            expect(response).to be_truthy
+        end
+    end
+  
+    describe "GET #profile" do
+        before :each do
+          sign_in
+        end
+        it "should post new test user" do
+        get :profile, id: 1
             expect(response).to be_truthy
         end
     end
